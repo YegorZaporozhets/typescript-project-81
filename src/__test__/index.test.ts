9d2ae978c9baea3f092c30e953a1b6d53a5be33e
ezaporozhets <ezaporozhets@oggettoweb.com>
2024-10-27 16:45:28 +0300

    Change test sturucture

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,7 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { describe, expect, test } from 'vitest';
+import Tag from '../generator/elements/tag';
+import sampleDataTag from '../__fixtures__/tag';
 import HexletCode from '../generator';
 import sampleData from '../__fixtures__/for-form';
+
+describe('Test Tag generator', () => {
+  sampleDataTag.forEach(({
+    tag, slot, attributes, resultString,
+  }) => {
+    test(`Create tag "${tag}"`, () => {
+      expect(new Tag(tag, attributes, slot).toString()).toBe(resultString);
+    });
+  });
+});
 
 describe('Test forForm:', () => {
   test('Empty Form', () => {
